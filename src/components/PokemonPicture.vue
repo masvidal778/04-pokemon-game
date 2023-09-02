eb55a1b796b7d77c34ff1d5ff0b22f68d7ad3536
masvidal778 <nuria.masvidal@gmail.com>
2023-09-02 20:29:48 +0200

    Designing pokemon picture and options components

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -7,9 +7,38 @@ export default defineComponent({
 </script>
 
 <template>
-<h3>Pokemon picture</h3>
+  <div class="pokemon-container">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         class="hidden-pokemon"
+         alt="pokemon">
+
+     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         class="fade-in"
+         alt="pokemon"
+    >
+  </div>
+
 </template>
 
 <style scoped>
 
+.pokemon-container {
+  height: 200px;
+}
+
+img {
+  height: 200px;
+  position: absolute;
+  width: 100vw;
+  margin-left: -50vw;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+
+.hidden-pokemon{
+  filter: brightness(0);
+}
 </style>
