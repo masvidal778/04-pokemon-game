e4554ac4fd5e7b942228536eb5a75ddb94eedc8b
masvidal778 <nuria.masvidal@gmail.com>
2023-09-05 02:05:53 +0200

    tested getPokemonOptions

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,7 +1,7 @@
 import pokemonApi from "@/api/pokemonApi";
 
 
-const getPokemons = () => {
+export const getPokemons = () => {
 
     const pokemonsArr = Array.from(Array(650))
 
@@ -10,7 +10,7 @@ const getPokemons = () => {
 }
 
 
-const getPokemonOptions = async () => {
+export const getPokemonOptions = async () => {
 
     const mixedPokemons = getPokemons()
                                        .sort( () => Math.random() -0.5 )
@@ -20,7 +20,7 @@ const getPokemonOptions = async () => {
     return pokemons
 }
 
-const getPokemonNames = async ( [a, b, c, d] ) => {
+export const getPokemonNames = async ( [a, b, c, d] ) => {
 
     //const resp = await pokemonApi.get(`/1`)
     //console.log(resp.data.name, resp.data.id)
---
+++ tests/unit/helpers/getPokemonOptions.spec.js
@@ -0,0 +1,55 @@
+import getPokemonOptions, {getPokemons, getPokemonNames} from "@/helpers/getPokemonOptions";
+
+describe('getPokemonOptions helpers', () => {
+
+    test('ha de tornar un array de nombres', () => {
+
+        const pokemons = getPokemons()
+
+        expect(pokemons.length).toBe(650)
+        expect(pokemons[0]).toBe(1)
+        expect(pokemons[500]).toBe(501)
+        expect(pokemons[649]).toBe(650)
+
+    })
+
+    test('ha de retornar un array de 4 elements amb noms de Pokemon', async () => {
+
+        const pokemons = await getPokemonNames([1, 2, 3, 4])
+
+        expect(pokemons).toStrictEqual([
+            {"id": 1, "name": "bulbasaur"},
+            {"id": 2, "name": "ivysaur"},
+            {"id": 3, "name": "venusaur"},
+            {"id": 4, "name": "charmander"}
+        ])
+
+    })
+
+    test('getPokemonOptions ha de retornar un array barrejat', async () => {
+
+        const pokemons = await getPokemonOptions()
+
+        expect(pokemons.length).toBe(4)
+        expect(pokemons).toEqual([
+            {
+                name: expect.any(String),
+                id: expect.any(Number)
+            },
+            {
+                name: expect.any(String),
+                id: expect.any(Number)
+            },
+            {
+                name: expect.any(String),
+                id: expect.any(Number)
+            },
+            {
+                name: expect.any(String),
+                id: expect.any(Number)
+            }
+        ])
+        
+    })
+
+})
